bb3259eb8c9e98581d4743b330a85834d17c85c6
Perkins <juston.perkins@williams.com>
2019-11-18 15:57:36 -0700

    Added a add ingredient and remove button

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -3,20 +3,22 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // grabs an object and turns it into an array
     let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey}/>
         });
     })
+    // takes an array and turns into another
     .reduce((arr,el) => {
         return arr.concat(el)
     }, [] );
-    
+    // ----------------------------------------------------------
     if (transformedIngredients.length === 0) {
         transformedIngredients = <p>Please add some ingredients</p>
     }
-    console.log(transformedIngredients);
+    
 
     return (
         <div className={classes.Burger}>
