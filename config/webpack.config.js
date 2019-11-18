ab71f2c58fb87423529c7cb31c289923ecd16bec
Perkins <juston.perkins@williams.com>
2019-11-18 10:49:14 -0700

    updated css

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -90,11 +90,7 @@ module.exports = function(webpackEnv) {
       },
       {
         loader: require.resolve('css-loader'),
-        options: {
-          importLoaders:1,
-          modules: true,
-          localIdentName: '[name]__[local]__[hash:base64:5]'
-        },
+        options:
         cssOptions,
       },
       {
